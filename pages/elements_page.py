fe95d150540c36a47ff6e441a6cb98431e0c3e3a
Your Name <knisch404@gmail.com>
2023-01-25 16:39:21 +0300

    commit -m added TRY to fix my EC problems

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -10,7 +10,7 @@ class TextBoxPage(BasePage):
     def fill_all_fields(self):
         # self.element_is_visible(self.locators.SUBMIT_IF_HAVE).click()
         # self.element_is_visible(self.locators.FULL_NAME).send_kys('')
-        self.element_is_visible(self.locators.EMAIL).send_kys('LarryFram.r.10.1.9.93@gmail.com')
+        self.element_is_present(self.locators.EMAIL).send_kys('LarryFram.r.10.1.9.93@gmail.com')
         self.element_is_visible(self.locators.FULL_PASSWORD).send_kys('yWGTseV3Qi3dUMZ')
         # self.element_is_visible(self.locators.CURRENT_ADDRESS).send_kys()
         # self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_kys()
